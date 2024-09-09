6f1af2079f3b88e1bb4949929da0c7a3d44681cd
edaniel-valencia <edanielvalenciam@gmail.com>
2024-09-09 10:32:17 -0500

    user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,12 +3,9 @@ import { CreateUser, LoginUser, ReadUser } from "../controllers/user";
 
 const router = Router();
 
-<<<<<<< HEAD
 router.get("/api/user/read", ReadUser)
 router.post("/api/user/create", CreateUser)
-=======
 router.post("/api/user/register", CreateUser)
->>>>>>> 13463c3c35e991302bc3006ca87468f85b5b63f6
 router.post("/api/user/login", LoginUser)
 
 
